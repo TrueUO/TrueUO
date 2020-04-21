d37ccc85bdddcff087b57e0515d6154a87a30a06
kevin-10 <kevin_bourne10@yahoo.com>
2020-04-21 15:21:27 -0700

    Creatures now defend themselves against summons casted by a young player Fixed issue with repair bench in use when its not in Use Fixed reward options gump

--- Scripts/Services/Craft/Core/Repair.cs
+++ Scripts/Services/Craft/Core/Repair.cs
@@ -179,7 +179,7 @@ namespace Server.Engines.Craft
                     if (tool.Charges == 0)
                     {
                         from.SendLocalizedMessage(1019073);// This item is out of charges.
-                        m_Addon.Using = false;
+                        m_Addon.User = null;
                         return;
                     }
 
@@ -210,7 +210,7 @@ namespace Server.Engines.Craft
                         if (!CheckDeed(from))
                         {
                             if (m_Addon != null)
-                                m_Addon.Using = false;
+                                m_Addon.User = null;
 
                             return;
                         }
@@ -220,7 +220,7 @@ namespace Server.Engines.Craft
                             from.SendLocalizedMessage(500426); // You can't repair that.
 
                             if (m_Addon != null)
-                                m_Addon.Using = false;
+                                m_Addon.User = null;
 
                             return;
                         }
