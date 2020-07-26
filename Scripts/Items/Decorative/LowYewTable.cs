b4c6e08f59e6159799b2396c03f84874439ae9d2
Argalep <argalep@quingis.com>
2020-07-26 23:04:47 +0300

    * add Spring Decor Collection

---
+++ Scripts/Items/Decorative/LowYewTable.cs
@@ -0,0 +1,40 @@
+using System;
+namespace Server.Items
+{
+    public class LowYewTable : Item
+    {
+        public override int LabelNumber { get { return 1075502; } } // Low Yew Table
+
+        [Constructable]
+        public LowYewTable()
+            : base(0x281A)
+        {
+            LootType = LootType.Blessed;
+            Hue = 1192;
+        }
+
+        public LowYewTable(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void GetProperties(ObjectPropertyList list)
+        {
+            base.GetProperties(list);
+
+            list.Add(1011032); // Yew
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            reader.ReadInt();
+        }
+    }
+}
