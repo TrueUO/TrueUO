5e1b7d383900162a41337ef6ef7f5aa2a247f5ce
TrueUO <knight.daniel.r@gmail.com>
2020-09-12 13:56:29 -0400

    Change SaveStrategy back to the way RunUO mostly had it. https://github.com/runuo/runuo/blob/master/Server/Persistence/SaveStrategy.cs

--- Server/Persistence/SaveStrategy.cs
+++ Server/Persistence/SaveStrategy.cs
@@ -3,35 +3,19 @@ namespace Server
 	public abstract class SaveStrategy
 	{
 		public abstract string Name { get; }
-		public static SaveStrategy Acquire()
-		{
-			if (Core.MultiProcessor)
-			{
-				int processorCount = Core.ProcessorCount;
+        public static SaveStrategy Acquire()
+        {
+            if (Core.MultiProcessor)
+            {
+                return new DualSaveStrategy();
+            }
+            else
+            {
+                return new StandardSaveStrategy();
+            }
+        }
 
-#if DynamicSaveStrategy
-                if (processorCount > 2)
-                {
-                    return new DynamicSaveStrategy();
-                }
-#else
-				if (processorCount > 16)
-				{
-					return new ParallelSaveStrategy(processorCount);
-				}
-#endif
-				else
-				{
-					return new DualSaveStrategy();
-				}
-			}
-			else
-			{
-				return new StandardSaveStrategy();
-			}
-		}
-
-		public abstract void Save(SaveMetrics metrics, bool permitBackgroundWrite);
+        public abstract void Save(SaveMetrics metrics, bool permitBackgroundWrite);
 
 		public abstract void ProcessDecay();
 	}
