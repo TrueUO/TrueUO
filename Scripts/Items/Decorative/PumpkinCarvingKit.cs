28116a6805333f28d2be39f6764b1189f2b8f9f0
TrueUO <knight.daniel.r@gmail.com>
2020-09-25 09:50:15 -0400

    Fixes bug with PumpkinCarvingKit.cs

--- Scripts/Items/Decorative/PumpkinCarvingKit.cs
+++ Scripts/Items/Decorative/PumpkinCarvingKit.cs
@@ -63,15 +63,17 @@ namespace Server.Items
                 {
                     BaseCarvablePumpkin pumpkin = (BaseCarvablePumpkin)targeted;
 
-                    from.PlaySound(0x249);
+                    if (pumpkin.CarvedBy == null)
+                    {
+                        from.PlaySound(0x249);
 
-                    pumpkin.ItemID = pumpkin.PumpkinDefinition[Utility.Random(pumpkin.PumpkinDefinition.Length)].UnlitItemID;
-                    pumpkin.CarvedBy = from.Name;
+                        pumpkin.ItemID = pumpkin.PumpkinDefinition[Utility.Random(pumpkin.PumpkinDefinition.Length)].UnlitItemID;
+                        pumpkin.CarvedBy = from.Name;
 
-                    from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1154339); // *You carefully carve the pumpkin*
+                        from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1154339); // *You carefully carve the pumpkin*
 
-                    m_Item.Delete();
-
+                        m_Item.Delete();
+                    }
                 }
                 else
                 {
