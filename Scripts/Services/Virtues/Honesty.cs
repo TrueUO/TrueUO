5c461afb825d09311673a0b70a8ca5c9b7d80cd1
TrueUO <knight.daniel.r@gmail.com>
2020-09-26 13:10:43 -0400

    Sonar Fixes

--- Scripts/Services/Virtues/Honesty.cs
+++ Scripts/Services/Virtues/Honesty.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Server.Engines.CannedEvil;
 using Server.Items;
