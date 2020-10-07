cd613677945735e0fe78579271a40f148c6f2c41
TrueUO <staff@trueuo.com>
2020-10-07 17:45:27 -0400

    Update DolphinMailbox.cs

--- Scripts/Items/StoreBought/DolphinMailbox.cs
+++ Scripts/Items/StoreBought/DolphinMailbox.cs
@@ -1,7 +1,7 @@
 namespace Server.Items
 {
     [Furniture]
-    public class DolphinMailbox : Mailbox, IFlipable
+    public class DolphinMailbox : Mailbox
     {
         public override int LabelNumber => 1113927;  // Mailbox
         public override int DefaultGumpID => 0x6D3;	
