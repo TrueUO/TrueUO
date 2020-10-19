007fcc66dd0c3438da57cab115f8b3446489c156
Dan <staff@trueuo.com>
2020-10-18 22:07:21 -0400

    Sonar

--- Scripts/Services/XmlSpawner/XmlSpawnerSkillCheck.cs
+++ Scripts/Services/XmlSpawner/XmlSpawnerSkillCheck.cs
@@ -1,12 +1,10 @@
 // Depreciated - Removing slowly 10/18/2020 - Dan
-
 namespace Server.Services.XmlSpawner
 {
     public class XmlSpawnerSkillCheck
     {
         public class RegisteredSkill
         {
-            private const int MaxSkills = 52;
             public const SkillName Invalid = (SkillName)(-1);
         }
     }
