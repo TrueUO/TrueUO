3e290b54d3702eed4d1373a651ca88bc48383c84
Argalep <argalep@quingis.com>
2020-12-14 16:31:22 +0300

    sonar

--- Server/Item.cs
+++ Server/Item.cs
@@ -1483,7 +1483,7 @@ namespace Server
 					{
 						if (!ip.Movable || rpm == from || ip.Map == bounce.m_Map && ip.GetWorldLocation() == bounce.m_WorldLoc)
 						{
-                            if (ip is Container c && !c.CheckHold(from, ip, false, false))
+                            if (from != null && ip is Container c && !c.CheckHold(from, ip, false, false))
                             {
                                 MoveToWorld(from.Location, from.Map);
                             }
