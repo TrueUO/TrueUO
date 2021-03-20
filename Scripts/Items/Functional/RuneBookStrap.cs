a5a96d84cfbdf49c5e5fb5aa8b2f27cdee79edb2
Argalep <argalep@quingis.com>
2021-03-20 03:12:59 +0300

    fix straps

--- Scripts/Items/Functional/RuneBookStrap.cs
+++ Scripts/Items/Functional/RuneBookStrap.cs
@@ -1,19 +1,12 @@
-using Server.ContextMenus;
 using Server.Engines.VeteranRewards;
-using Server.Gumps;
-using Server.Multis;
-using Server.Network;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Server.Items
 {
-    public class RuneBookStrap : Container, ISecurable, IRewardItem, IDyable
+    public class RuneBookStrap : BaseContainer, IRewardItem, IDyable
     {
         public override int LabelNumber => 1126807; // book strap
-
-        [CommandProperty(AccessLevel.GameMaster)]
-        public SecureLevel Level { get; set; }
 
         private bool m_IsRewardItem;
 
@@ -34,7 +27,6 @@ namespace Server.Items
         {
             LootType = LootType.Blessed;
             Weight = 3.0;
-            Level = SecureLevel.CoOwners;
         }
 
         public virtual bool Dye(Mobile from, DyeTub sender)
@@ -65,57 +57,9 @@ namespace Server.Items
             list.Add(1072210, "30"); // Weight reduction: ~1_PERCENTAGE~%            
         }
 
-        public override void OnDoubleClick(Mobile from)
-        {
-            if (from.AccessLevel >= AccessLevel.GameMaster)
-                base.OnDoubleClick(from);
-
-            if (!from.InRange(GetWorldLocation(), 2))
-            {
-                from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
-            }
-            else
-            {
-                base.OnDoubleClick(from);
-            }
-        }
-
-        public bool CheckAccessible(Mobile from, Item item)
-        {
-            if (from.AccessLevel >= AccessLevel.GameMaster)
-                return true; // Staff can access anything
-
-            BaseHouse house = BaseHouse.FindHouseAt(item);
-
-            if (house == null)
-                return false;
-
-            switch (Level)
-            {
-                case SecureLevel.Owner: return house.IsOwner(from);
-                case SecureLevel.CoOwners: return house.IsCoOwner(from);
-                case SecureLevel.Friends: return house.IsFriend(from);
-                case SecureLevel.Anyone: return true;
-                case SecureLevel.Guild: return house.IsGuildMember(from);
-            }
-
-            return false;
-        }
-
-        public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
-        {
-            base.GetContextMenuEntries(from, list);
-
-            SetSecureLevelEntry.AddTo(from, this, list);
-        }
-
         public bool IsAccept(Item item)
         {
-            foreach (Type type in _AcceptList)
-                if (item.GetType().IsSubclassOf(type))
-                    return true;
-
-            return false;
+            return _AcceptList.Any(t => t == item.GetType());
         }
 
         private readonly Type[] _AcceptList =
@@ -125,12 +69,6 @@ namespace Server.Items
 
         public override bool OnDragDrop(Mobile from, Item dropped)
         {
-            if (!CheckAccessible(from, this))
-            {
-                PrivateOverheadMessage(MessageType.Regular, 946, 1010563, from.NetState); // This container is secure.
-                return false;
-            }
-
             if (!IsAccept(dropped))
             {
                 from.SendLocalizedMessage(1074836); // The container cannot hold that type of object.
@@ -142,12 +80,6 @@ namespace Server.Items
 
         public override bool OnDragDropInto(Mobile from, Item item, Point3D p)
         {
-            if (!CheckAccessible(from, this))
-            {
-                PrivateOverheadMessage(MessageType.Regular, 946, 1010563, from.NetState); // This container is secure.
-                return false;
-            }
-
             if (!IsAccept(item))
             {
                 from.SendLocalizedMessage(1074836); // The container cannot hold that type of object.
@@ -180,7 +112,6 @@ namespace Server.Items
             base.Serialize(writer);
             writer.Write(0);
 
-            writer.Write((int)Level);
             writer.Write(m_IsRewardItem);
         }
 
@@ -189,7 +120,6 @@ namespace Server.Items
             base.Deserialize(reader);
             reader.ReadInt();
 
-            Level = (SecureLevel)reader.ReadInt();
             m_IsRewardItem = reader.ReadBool();
         }
     }
