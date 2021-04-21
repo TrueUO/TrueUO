1206fcd930afa6e0f700cb726783955a167b8956
Dan <staff@trueuo.com>
2021-04-21 12:29:33 -0400

    Crash fix.

--- Scripts/Services/Pet Training/SpecialAbility.cs
+++ Scripts/Services/Pet Training/SpecialAbility.cs
@@ -915,21 +915,24 @@ namespace Server.Mobiles
 
                     bool any = false;
 
-                    for (var i = 0; i < d.Uses.Length; i++)
+                    if (d != null)
                     {
-                        var type = d.Uses[i];
-
-                        if (type == bc.GetType())
+                        for (var i = 0; i < d.Uses.Length; i++)
                         {
-                            any = true;
+                            var type = d.Uses[i];
+
+                            if (type == bc.GetType())
+                            {
+                                any = true;
+                                break;
+                            }
+                        }
+
+                        if (d.Uses != null && any)
+                        {
+                            def = d;
                             break;
                         }
-                    }
-
-                    if (d.Uses != null && any)
-                    {
-                        def = d;
-                        break;
                     }
                 }
 
