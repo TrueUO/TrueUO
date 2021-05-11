b33e8b1c2cd9a62072b041ed96da0201b9844d33
Argalep <argalep@quingis.com>
2021-05-11 03:52:24 +0300

    fix

--- Scripts/Quests/TheBlackGate/Items/AnkhNecklace.cs
+++ Scripts/Quests/TheBlackGate/Items/AnkhNecklace.cs
@@ -1,5 +1,4 @@
 using Server.Gumps;
-using Server.Network;
 
 namespace Server.Items
 {
@@ -20,24 +19,15 @@ namespace Server.Items
 
         public override void OnDoubleClick(Mobile from)
         {
-            Gump g = new Gump(100, 100);
+            QuestRewardGump g = new QuestRewardGump(this, from)
+            {
+                Title = "An Ankh Necklace",
+                Description = "Recovered from a Fallen Ranger in Hythloth",
+                Line1 = "You recovered the spirit of the fallen ranger in Hytloth for Shamino.",
+                Line2 = "You have proven your own Spirituality in performing this solemn deed, and for that Shamino has given you the necklace."
+            };
 
-            g.AddPage(0);
-
-            g.AddBackground(0, 0, 480, 320, 0x6DB);
-            g.AddSpriteImage(24, 24, 0x474, 60, 60, 108, 108);
-            g.AddImage(15, 15, 0xA9F);
-            g.AddImageTiledButton(22, 22, 0x176F, 0x176F, 0x0, GumpButtonType.Page, 0, ItemID, Hue, 33, 43);
-            g.AddHtml(150, 15, 320, 22, "<BASEFONT COLOR=#D5D52A><DIV ALIGN=CENTER>An Ankh Necklace</DIV>", false, false);
-            g.AddHtml(150, 46, 320, 44, "<BASEFONT COLOR=#AABFD4><DIV ALIGN=CENTER>Recovered from a Fallen Ranger in Hythloth</DIV>", false, false);
-            g.AddHtml(150, 99, 320, 98, "<BASEFONT COLOR=#DFDFDF>You recovered the spirit of the fallen ranger in Hytloth for Shamino.", false, false);
-            g.AddHtml(150, 197, 320, 98, "<BASEFONT COLOR=#DFDFDF>You have proven your own Spirituality in performing this solemn deed, and for that Shamino has given you the necklace.", false, false);
-
-            from.CloseGump(typeof(Gump));
-            from.SendGump(g);
-
-            from.PrivateOverheadMessage(MessageType.Regular, 0x47E, 1157722, "its origin", from.NetState); // *Your proficiency in ~1_SKILL~ reveals more about the item*
-            from.SendSound(from.Female ? 0x30B : 0x41A);
+            g.RenderString(from);
         }
 
         public override void Serialize(GenericWriter writer)
--- Scripts/Quests/TheBlackGate/Items/MugOfPurpleAle.cs
+++ Scripts/Quests/TheBlackGate/Items/MugOfPurpleAle.cs
@@ -1,5 +1,4 @@
 using Server.Gumps;
-using Server.Network;
 
 namespace Server.Items
 {
@@ -24,23 +23,15 @@ namespace Server.Items
 
         public override void OnDoubleClick(Mobile from)
         {
-            Gump g = new Gump(100, 100);
-            g.AddPage(0);
+            QuestRewardGump g = new QuestRewardGump(this, from)
+            {
+                Title = "A Frothy Mug of Purple Ale",
+                Description = "Shared by Dupre",
+                Line1 = "A specialty of the Keg and Anchor, Dupre is known to enjoy a mug or two of Purple Ale.",
+                Line2 = "In proving your Honor to Dupre in battle against the daemon Arcadion, he has shared this mug of ale with you."
+            };
 
-            g.AddBackground(0, 0, 480, 320, 0x6DB);
-            g.AddSpriteImage(24, 24, 0x474, 60, 60, 108, 108);
-            g.AddImage(15, 15, 0xA9F);
-            g.AddImageTiledButton(22, 22, 0x176F, 0x176F, 0x0, GumpButtonType.Page, 0, ItemID, Hue, 33, 38);
-            g.AddHtml(150, 15, 320, 22, "<BASEFONT COLOR=#D5D52A><DIV ALIGN=CENTER>A Frothy Mug of Purple Ale</DIV>", false, false);
-            g.AddHtml(150, 46, 320, 44, "<BASEFONT COLOR=#AABFD4><DIV ALIGN=CENTER>Shared by Dupre</DIV>", false, false);
-            g.AddHtml(150, 99, 320, 98, "<BASEFONT COLOR=#DFDFDF>A specialty of the Keg and Anchor, Dupre is known to enjoy a mug or two of Purple Ale.", false, false);
-            g.AddHtml(150, 197, 320, 98, "<BASEFONT COLOR=#DFDFDF>In proving your Honor to Dupre in battle against the daemon Arcadion, he has shared this mug of ale with you.", false, false);
-
-            from.CloseGump(typeof(Gump));
-            from.SendGump(g);
-
-            from.PrivateOverheadMessage(MessageType.Regular, 0x47E, 1157722, "its origin", from.NetState); // *Your proficiency in ~1_SKILL~ reveals more about the item*
-            from.SendSound(from.Female ? 0x30B : 0x41A);
+            g.RenderString(from);
         }
 
         public override void Serialize(GenericWriter writer)
