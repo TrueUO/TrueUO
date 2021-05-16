31784ebbb4ba71110a576b24462ebc00de1bec78
Dan <staff@trueuo.com>
2021-05-16 14:37:59 -0400

    Sonar

--- Scripts/Items/Tools/BraceletOfBinding.cs
+++ Scripts/Items/Tools/BraceletOfBinding.cs
@@ -374,26 +374,25 @@ namespace Server.Items
                 m_From.Frozen = false;
 
                 if (m_Bracelet.Deleted || m_From.Deleted)
-                    return;
-
-                if (m_Bracelet.CheckUse(m_From, false))
                 {
-                    if (m_Bracelet.Bound.RootParent is Mobile boundRoot)
+                    return;
+                }
+
+                if (m_Bracelet.CheckUse(m_From, false) && m_Bracelet.Bound.RootParent is Mobile boundRoot)
+                {
+                    m_Bracelet.Charges--;
+
+                    BaseCreature.TeleportPets(m_From, boundRoot.Location, boundRoot.Map, true);
+
+                    m_From.PlaySound(0x1FC);
+                    m_From.MoveToWorld(boundRoot.Location, boundRoot.Map);
+                    m_From.PlaySound(0x1FC);
+
+                    BaseHouse house = BaseHouse.FindHouseAt(boundRoot);
+
+                    if (house != null)
                     {
-                        m_Bracelet.Charges--;
-
-                        BaseCreature.TeleportPets(m_From, boundRoot.Location, boundRoot.Map, true);
-
-                        m_From.PlaySound(0x1FC);
-                        m_From.MoveToWorld(boundRoot.Location, boundRoot.Map);
-                        m_From.PlaySound(0x1FC);
-
-                        BaseHouse house = BaseHouse.FindHouseAt(boundRoot);
-
-                        if (house != null)
-                        {
-                            m_From.SendLocalizedMessage(1070905); // Strong magics have redirected you to a safer location!
-                        }
+                        m_From.SendLocalizedMessage(1070905); // Strong magics have redirected you to a safer location!
                     }
                 }
             }
