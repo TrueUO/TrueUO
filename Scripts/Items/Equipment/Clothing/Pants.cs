54176ec4faf002c10d4c6c007fac2097a6401414
Dan <staff@trueuo.com>
2021-06-18 19:14:39 -0400

    -
    The male and female versions are not needed and were only used by NPCs. Force re-spawn those.

--- Scripts/Items/Equipment/Clothing/Pants.cs
+++ Scripts/Items/Equipment/Clothing/Pants.cs
@@ -292,79 +292,17 @@ namespace Server.Items
             if (parent is Mobile mobile)
             {
                 if (mobile.Female)
+                {
                     ItemID = 0x0409;
+                }
                 else
+                {
                     ItemID = 0x040A;
+                }
             }
         }
 
         public GargishClothLegs(Serial serial)
-            : base(serial)
-        {
-        }
-
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-            reader.ReadInt();
-        }
-    }
-
-    public class FemaleGargishClothLegs : BaseClothing
-    {
-        [Constructable]
-        public FemaleGargishClothLegs()
-            : this(0)
-        {
-        }
-
-        [Constructable]
-        public FemaleGargishClothLegs(int hue)
-            : base(0x0409, Layer.Pants, hue)
-        {
-            Weight = 2.0;
-        }
-
-        public FemaleGargishClothLegs(Serial serial)
-            : base(serial)
-        {
-        }
-
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-            reader.ReadInt();
-        }
-    }
-
-    public class MaleGargishClothLegs : BaseClothing
-    {
-        [Constructable]
-        public MaleGargishClothLegs()
-            : this(0)
-        {
-        }
-
-        [Constructable]
-        public MaleGargishClothLegs(int hue)
-            : base(0x040A, Layer.Pants, hue)
-        {
-            Weight = 2.0;
-        }
-
-        public MaleGargishClothLegs(Serial serial)
             : base(serial)
         {
         }
