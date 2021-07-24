65474dd3c3a5103f14f3031b1e400807b2627a9a
Virulent16 <mwa2792@gmail.com>
2021-07-24 01:01:21 -0500

    -Fixed spawn errors that were made after, CreateWorld command. -Added missing mobile, in one of the errors, Jwilson. Will post link. -Fixed spawn in Jhelom, hires were not correct, compared with multiple shard on OSI. Only hires available in Dueling Pit, are fighters.

---
+++ Scripts/Mobiles/Named/Jwilson.cs
@@ -0,0 +1,79 @@
+namespace Server.Mobiles
+{
+    [CorpseName("Jwilson's corpse")]
+    public class Jwilson : BaseCreature
+    {
+        [Constructable]
+        public Jwilson()
+            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+        {
+            Name = "a Jwilson";
+            Body = 51;
+            BaseSoundID = 456;
+
+            Hue = Utility.RandomSlimeHue();
+
+            SetStr(22, 34);
+            SetDex(16, 21);
+            SetInt(16, 20);
+
+            SetHits(15, 19);
+
+            SetDamage(1, 5);
+
+            SetDamageType(ResistanceType.Physical, 100);
+
+            SetResistance(ResistanceType.Physical, 5, 10);
+            SetResistance(ResistanceType.Poison, 10, 20);
+
+            SetSkill(SkillName.Poisoning, 30.1, 50.0);
+            SetSkill(SkillName.MagicResist, 15.1, 20.0);
+            SetSkill(SkillName.Tactics, 19.3, 34.0);
+            SetSkill(SkillName.Wrestling, 19.3, 34.0);
+
+            Fame = 300;
+            Karma = -300;
+
+            Tamable = false;
+            ControlSlots = 1;
+            MinTameSkill = 23.1;
+        }
+
+        public Jwilson(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override Poison PoisonImmune => Poison.Lesser;
+        public override Poison HitPoison => Poison.Lesser;
+        public override FoodType FavoriteFood => FoodType.Meat | FoodType.Fish | FoodType.FruitsAndVegies | FoodType.GrainsAndHay | FoodType.Eggs;
+        public override void GenerateLoot()
+        {
+            AddLoot(LootPack.Poor);
+            AddLoot(LootPack.Gems);
+        }
+
+        public override bool CheckMovement(Direction d, out int newZ)
+        {
+            if (!base.CheckMovement(d, out newZ))
+                return false;
+
+            if (Region.IsPartOf("Underworld") && newZ > Location.Z)
+                return false;
+
+            return true;
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(1);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            reader.ReadInt();
+        }
+    }
+}
