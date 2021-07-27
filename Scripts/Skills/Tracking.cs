e5df4cddc405e2d126c6cc06677536779f58ec50
Dan <staff@trueuo.com>
2021-07-27 08:39:03 -0400

    Last

--- Scripts/Skills/Tracking.cs
+++ Scripts/Skills/Tracking.cs
@@ -609,22 +609,17 @@ namespace Server.SkillHandlers
 
         private static bool IsAnimal(Mobile m)
         {
-            return m.Body.IsAnimal && !(m.Region.IsPartOf<Regions.HouseRegion>() && (m is BaseVendor || m is PlayerVendor || m is BaseCreature bc && bc.Blessed));
+            return m.Body.IsAnimal;
         }
 
         private static bool IsMonster(Mobile m)
         {
-            return (
-                    !m.Player && m.Body.IsHuman && m is BaseCreature bc && bc.IsAggressiveMonster
-                    || m.Body.IsMonster
-                    || TrackedNecro(m)
-                )
-                && !(m.Region.IsPartOf<Regions.HouseRegion>() && (m is BaseVendor || m is PlayerVendor));
+            return !m.Player && m.Body.IsHuman && m is BaseCreature bc && bc.IsAggressiveMonster || m.Body.IsMonster || TrackedNecro(m);
         }
 
         private static bool IsHumanNPC(Mobile m)
         {
-            return !m.Player && m.Body.IsHuman && m is BaseCreature bc && !bc.IsAggressiveMonster || TrackedThief(m) || m.Region.IsPartOf<Regions.HouseRegion>() && (m is BaseVendor || m is PlayerVendor);
+            return !m.Player && m.Body.IsHuman && m is BaseCreature bc && !bc.IsAggressiveMonster || TrackedThief(m);
         }
 
         private static bool IsPlayer(Mobile m)
