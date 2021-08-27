86c5f5208e25c3ee1bba409b00a89267e791a240
Dan <staff@trueuo.com>
2021-08-27 12:41:00 -0400

    Removes Unneeded Reports
    - BOD documentation, never updated past original old system, is no longer supported.
    - SetBodyGump removed. Required UO3D files to work correctly. (mul vs UOP)

--- Scripts/Gumps/Props/SetGump.cs
+++ Scripts/Gumps/Props/SetGump.cs
@@ -38,7 +38,7 @@ namespace Server.Gumps
         public static readonly int BorderSize = PropsConfig.BorderSize;
         private static readonly int EntryWidth = 212;
         private static readonly int TotalWidth = OffsetSize + EntryWidth + OffsetSize + SetWidth + OffsetSize;
-        private static readonly int TotalHeight = OffsetSize + (2 * (EntryHeight + OffsetSize));
+        private static readonly int TotalHeight = OffsetSize + 2 * (EntryHeight + OffsetSize);
         private static readonly int BackWidth = BorderSize + TotalWidth + BorderSize;
         private static readonly int BackHeight = BorderSize + TotalHeight + BorderSize;
         private readonly PropertyInfo m_Property;
@@ -60,7 +60,6 @@ namespace Server.Gumps
 
             bool canNull = !prop.PropertyType.IsValueType;
             bool canDye = prop.IsDefined(typeof(HueAttribute), false);
-            bool isBody = prop.IsDefined(typeof(BodyAttribute), false);
 
             object val = prop.GetValue(m_Object, null);
             string initialText;
@@ -78,15 +77,13 @@ namespace Server.Gumps
                 0,
                 0,
                 BackWidth,
-                BackHeight + (canNull ? (EntryHeight + OffsetSize) : 0) + (canDye ? (EntryHeight + OffsetSize) : 0) +
-                (isBody ? (EntryHeight + OffsetSize) : 0),
+                BackHeight + (canNull ? EntryHeight + OffsetSize : 0) + (canDye ? EntryHeight + OffsetSize : 0),
                 BackGumpID);
             AddImageTiled(
                 BorderSize,
                 BorderSize,
                 TotalWidth - (OldStyle ? SetWidth + OffsetSize : 0),
-                TotalHeight + (canNull ? (EntryHeight + OffsetSize) : 0) + (canDye ? (EntryHeight + OffsetSize) : 0) +
-                (isBody ? (EntryHeight + OffsetSize) : 0),
+                TotalHeight + (canNull ? EntryHeight + OffsetSize : 0) + (canDye ? EntryHeight + OffsetSize : 0),
                 OffsetGumpID);
 
             int x = BorderSize + OffsetSize;
@@ -140,21 +137,6 @@ namespace Server.Gumps
 
                 AddButton(x + SetOffsetX, y + SetOffsetY, SetButtonID1, SetButtonID2, 3, GumpButtonType.Reply, 0);
             }
-
-            if (isBody)
-            {
-                x = BorderSize + OffsetSize;
-                y += EntryHeight + OffsetSize;
-
-                AddImageTiled(x, y, EntryWidth, EntryHeight, EntryGumpID);
-                AddLabelCropped(x + TextOffsetX, y, EntryWidth - TextOffsetX, EntryHeight, TextHue, "Body Picker");
-                x += EntryWidth + OffsetSize;
-
-                if (SetGumpID != 0)
-                    AddImageTiled(x, y, SetWidth, EntryHeight, SetGumpID);
-
-                AddButton(x + SetOffsetX, y + SetOffsetY, SetButtonID1, SetButtonID2, 4, GumpButtonType.Reply, 0);
-            }
         }
 
         public override void OnResponse(NetState sender, RelayInfo info)
@@ -204,16 +186,6 @@ namespace Server.Gumps
                         shouldSend = false;
 
                         m_Mobile.SendHuePicker(new InternalPicker(m_Property, m_Mobile, m_Object, m_Stack, m_Page, m_List));
-
-                        break;
-                    }
-                case 4: // Body Picker
-                    {
-                        toSet = null;
-                        shouldSet = false;
-                        shouldSend = false;
-
-                        m_Mobile.SendGump(new SetBodyGump(m_Property, m_Mobile, m_Object, m_Stack, m_Page, m_List));
 
                         break;
                     }
