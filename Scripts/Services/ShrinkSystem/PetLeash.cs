59dfde871730b01e5be15aaca70c640299528fcd
Dan <staff@trueuo.com>
2022-01-04 21:09:02 -0500

    Shrink System
    - Original Shrink system design by Xanthos.
    - Won't be merging this with TrueUO but will keep this branch open for now.
    - Should solve issue with pets unlinking.
    - Super cleaned up and slimmed down.

---
+++ Scripts/Services/ShrinkSystem/PetLeash.cs
@@ -0,0 +1,151 @@
+using Server.Mobiles;
+using Server.Targeting;
+
+namespace Server.Services.ShrinkSystem
+{
+    public interface IShrinkTool
+    {
+        int ShrinkCharges { get; set; }
+    }
+
+	public class PetLeash : Item, IShrinkTool
+	{	
+		private int m_Charges = -1; // set to -1 for infinite uses
+
+		[CommandProperty( AccessLevel.GameMaster )]
+		public int ShrinkCharges
+		{
+			get => m_Charges;
+            set
+			{
+				if (0 == m_Charges || 0 == (m_Charges = value))
+                {
+                    Delete();
+                }
+                else
+                {
+                    InvalidateProperties();
+                }
+            }
+		}
+
+		[Constructable]
+		public PetLeash()
+            : base(0x1374)
+		{
+			Weight = 1.0;
+			Movable = true;
+			Name = "Pet Leash";
+			LootType = LootType.Blessed;
+		}
+
+		public PetLeash(Serial serial)
+            : base(serial)
+		{
+		}
+
+		public override void AddNameProperties(ObjectPropertyList list)
+		{
+			base.AddNameProperties(list);
+
+			if (m_Charges >= 0)
+            {
+                list.Add(1060658, "Charges\t{0}", m_Charges.ToString());
+            }
+        }
+
+		public override void OnDoubleClick(Mobile from)
+		{
+			bool isStaff = from.AccessLevel != AccessLevel.Player;
+
+			if (!IsChildOf(from.Backpack))
+            {
+                from.SendLocalizedMessage(1042001); // That must be in your pack for you to use it.
+            }
+            else if (isStaff || from.Skills[ SkillName.AnimalTaming ].Value >= ShrinkItem.TamingRequired)
+            {
+                from.Target = new ShrinkTarget(from, this);
+            }
+            else
+            {
+                from.SendMessage("You must have at least " + ShrinkItem.TamingRequired + " animal taming to use a pet leash.");
+            }
+        }
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write(0);
+
+			writer.Write(m_Charges);
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+            reader.ReadInt();
+
+			m_Charges = reader.ReadInt();
+		}
+	}
+
+    public class ShrinkTarget : Target
+	{
+		private readonly IShrinkTool _ShrinkTool;
+
+        public ShrinkTarget(Mobile from, IShrinkTool shrinkTool)
+            : base(10, false, TargetFlags.None)
+		{
+			_ShrinkTool = shrinkTool;
+
+            from.SendMessage("Target the pet you wish to shrink.");
+		}
+
+		protected override void OnTarget(Mobile from, object targeted)
+		{
+            if (!(targeted is BaseCreature pet)) // null check
+            {
+                from.SendMessage("That is not a pet!");
+            }
+            else if (!pet.Controlled)
+            {
+                from.SendMessage("You cannot not shrink wild creatures.");
+            }
+            else if (pet.ControlMaster != from)
+            {
+                from.SendMessage("That is not your pet.");
+            }
+            else if (pet.IsDeadPet)
+            {
+                from.SendMessage("You cannot shrink the dead!");
+            }
+            else if (pet.Summoned || pet.Allured || pet is BaseTalismanSummon)
+            {
+                from.SendMessage("You cannot shrink a summoned creature!");
+            }
+            else if (pet.Combatant != null && pet.InRange(pet.Combatant, 12) && pet.Map == pet.Combatant.Map || Spells.SpellHelper.CheckCombat(from))
+            {
+                from.SendMessage("You or your pet are engaged in combat; you cannot shrink it yet.");
+            }
+            else if ((pet is PackLlama || pet is PackHorse || pet is Beetle) && pet.Backpack != null && pet.Backpack.Items.Count > 0)
+            {
+                from.SendMessage("You must unload this pet's pack before it can be shrunk.");
+            }
+            else
+			{
+                IEntity p1 = new Entity(Serial.Zero, new Point3D(from.X, from.Y, from.Z), from.Map);
+				IEntity p2 = new Entity(Serial.Zero, new Point3D(from.X, from.Y, from.Z + 50 ), from.Map);
+
+                Effects.SendMovingParticles(p2, p1, ShrinkTable.Lookup(pet), 1, 0, true, false, 0, 3, 1153, 1, 0, EffectLayer.Head, 0x100);
+
+				from.PlaySound(492);
+				from.AddToBackpack(new ShrinkItem(pet));
+
+				if (_ShrinkTool != null && _ShrinkTool.ShrinkCharges > 0)
+                {
+                    _ShrinkTool.ShrinkCharges--;
+                }
+            }
+		}
+    }
+}
---
+++ Scripts/Services/ShrinkSystem/ShrinkItem.cs
@@ -0,0 +1,167 @@
+using Server.Mobiles;
+using Server.Spells;
+
+namespace Server.Services.ShrinkSystem
+{
+    public interface IShrinkItem
+    {
+        BaseCreature ShrunkenPet { get; }
+    }
+
+	public class ShrinkItem : Item, IShrinkItem
+	{
+        // Settings
+        public static double TamingRequired = 0; // set to zero for no skill requirement to use shrink tools
+
+        // Persisted
+        private Mobile m_Owner;
+		private BaseCreature m_Pet;
+
+		[CommandProperty( AccessLevel.GameMaster )]
+		public Mobile Owner
+		{
+			get => m_Owner; set { m_Owner = value; InvalidateProperties(); }
+		}
+
+		[CommandProperty( AccessLevel.GameMaster )]
+		public BaseCreature ShrunkenPet
+		{
+			get => m_Pet; set { m_Pet = value; InvalidateProperties(); }
+		}
+
+		public ShrinkItem(BaseCreature pet)
+            : base(ShrinkTable.Lookup(pet))
+		{
+			ShrinkPet(pet);
+
+            Name = "a shrunken pet";
+            Weight = 10;
+            Hue = m_Pet.Hue;
+            LootType = m_Pet.IsBonded ? LootType.Blessed : LootType.Regular;
+        }
+
+        public ShrinkItem(Serial serial)
+            : base(serial)
+        {
+        }
+
+		public override void OnDoubleClick(Mobile from)
+		{
+            if (!IsChildOf(from.Backpack))
+            {
+                from.SendLocalizedMessage(1042001); // That must be in your pack for you to use it.
+            }
+            else if (m_Pet == null || m_Pet.Deleted || ItemID == 0xFAA)
+            {
+                from.SendMessage("Due to unforeseen circumstances your pet is lost forever.");
+            }
+            else if (from.Followers + m_Pet.ControlSlots > from.FollowersMax)
+            {
+                from.SendMessage("You have to many followers to claim this pet.");
+            }
+            else if (SpellHelper.CheckCombat(from))
+            {
+                from.SendMessage("You cannot reclaim your pet while your fighting.");
+            }
+            else if (!m_Pet.CanBeControlledBy(from))
+            {
+                from.SendMessage("You do not have the required skills to control this pet.");
+            }
+            else
+            {
+                m_Pet.SetControlMaster(from);
+                m_Pet.IsStabled = false;
+
+                m_Pet.MoveToWorld(from.Location, from.Map);
+
+                if (from != m_Owner)
+                {
+                    m_Pet.IsBonded = false;
+                }
+
+                m_Pet = null;
+
+                Delete();
+            }
+        }
+
+		private void ShrinkPet(BaseCreature pet)
+		{
+			m_Pet = pet;
+            m_Owner = pet.ControlMaster;
+
+            m_Pet.ControlTarget = null;
+            m_Pet.ControlOrder = OrderType.Stay;
+            m_Pet.Internalize();
+
+			m_Pet.SetControlMaster(null);
+            m_Pet.SummonMaster = null;
+            m_Pet.Loyalty = BaseCreature.MaxLoyalty;
+            m_Pet.IsStabled = true;
+        }
+
+		public override void Delete()
+		{
+			if (m_Pet != null)	// Don't orphan pets on the internal map
+            {
+                m_Pet.Delete();
+            }
+
+            base.Delete();
+		}
+
+		public override void AddNameProperties(ObjectPropertyList list)
+		{
+			base.AddNameProperties(list);
+
+			if (null == m_Pet || m_Pet.Deleted)
+            {
+                return;
+            }
+
+            var wrestling = m_Pet.Skills[SkillName.Wrestling].Base;
+            var tactics = m_Pet.Skills[SkillName.Tactics].Base;
+            var anatomy = m_Pet.Skills[SkillName.Anatomy].Base;
+            var poisoning = m_Pet.Skills[SkillName.Poisoning].Base;
+            var magery = m_Pet.Skills[SkillName.Magery].Base;
+            var eval = m_Pet.Skills[SkillName.EvalInt].Base;
+            var resist = m_Pet.Skills[SkillName.MagicResist].Base;
+            var meditation = m_Pet.Skills[SkillName.Meditation].Base;
+            
+            list.Add(1060663, "Name\t{0} Breed: {1}", m_Pet.Name, m_Pet.GetType().Name);
+
+            if (m_Pet.IsBonded)
+            {
+                list.Add(1049608);
+            }
+
+            list.Add(1061640, m_Owner == null ? "nobody (WILD)" : m_Owner.Name); // Owner: ~1_OWNER~
+            list.Add(1060659, "Stats\tStrength {0}, Dexterity {1}, Intelligence {2}", m_Pet.RawStr, m_Pet.RawDex, m_Pet.RawInt);
+            list.Add(1060660, "Combat Skills\tWrestling {0}, Tactics {1}, Anatomy {2}, Poisoning {3}", wrestling, tactics, anatomy, poisoning);
+            list.Add(1060661, "Magic Skills\tMagery {0}, Eval Intel {1}, Magic Resist {2}, Meditation {3}", magery, eval, resist, meditation);
+        }
+
+        public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+            writer.Write(0); // version
+
+			writer.Write(m_Owner);
+			writer.Write(m_Pet);
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+            reader.ReadInt();
+
+            m_Owner = (PlayerMobile)reader.ReadMobile();
+            m_Pet = (BaseCreature)reader.ReadMobile();
+
+            if (m_Pet != null)
+            {
+                m_Pet.IsStabled = true;
+            }
+        }
+	}
+}
