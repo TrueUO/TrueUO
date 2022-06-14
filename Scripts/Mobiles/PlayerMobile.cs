22809fd8d1ac30c18bd1da28532f15e683b1092f
David Rath <adverserath@gmail.com>
2022-06-14 13:37:44 +0100

    Fix Provocation New Target

--- Scripts/Mobiles/PlayerMobile.cs
+++ Scripts/Mobiles/PlayerMobile.cs
@@ -826,6 +826,11 @@ namespace Server.Mobiles
 
             from.TargetLocked = true;
 
+            if (e.SkillID == 22)
+            {
+                Provocation.DeferredSecondaryTarget = true;
+            }
+
             if (e.SkillID == 35)
             {
                 AnimalTaming.DisableMessage = true;
@@ -835,6 +840,11 @@ namespace Server.Mobiles
             if (from.UseSkill(e.SkillID))
             {
                 from.Target?.Invoke(from, target);
+            }
+
+            if (e.SkillID == 22)
+            {
+                Provocation.DeferredSecondaryTarget = false;
             }
 
             if (e.SkillID == 35)
