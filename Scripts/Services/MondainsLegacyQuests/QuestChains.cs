d2e6d03afee9da46624a1d829258937d80581b95
Daniel Knight <knight.daniel.r@gmail.com>
2023-05-18 08:37:50 -0400

    1

--- Scripts/Services/MondainsLegacyQuests/QuestChains.cs
+++ Scripts/Services/MondainsLegacyQuests/QuestChains.cs
@@ -11,16 +11,13 @@ namespace Server.Engines.Quests
         Marauders = 1,
         UnfadingMemories = 2,
         DoughtyWarriors = 3,
-        HonorOfDeBoors = 4,
-        LaifemTheWeaver = 5,
-        CloakOfHumility = 6,
-        ValleyOfOne = 7,
-        MyrmidexAlliance = 8,
-        EodonianAlliance = 9,
-        FlintTheQuartermaster = 10,
-        RightingWrong = 11,
-        Ritual = 12,
-        Ritual2 = 13
+        LaifemTheWeaver = 4,
+        CloakOfHumility = 5,
+        ValleyOfOne = 6,
+        MyrmidexAlliance = 7,
+        EodonianAlliance = 8,
+        FlintTheQuartermaster = 9,
+        RightingWrong = 10
     }
 
     public class BaseChain
@@ -29,14 +26,13 @@ namespace Server.Engines.Quests
 
         static BaseChain()
         {
-            Chains = new Type[14][];
+            Chains = new Type[11][];
 
             Chains[(int)QuestChain.None] = new Type[] { };
 
             Chains[(int)QuestChain.Marauders] = new[] { typeof(MaraudersQuest), typeof(TheBrainsOfTheOperationQuest), typeof(TheBrawnQuest), typeof(TheBiggerTheyAreQuest) };
             Chains[(int)QuestChain.UnfadingMemories] = new[] { typeof(UnfadingMemoriesOneQuest), typeof(UnfadingMemoriesTwoQuest), typeof(UnfadingMemoriesThreeQuest) };
             Chains[(int)QuestChain.DoughtyWarriors] = new[] { typeof(DoughtyWarriorsQuest), typeof(DoughtyWarriors2Quest), typeof(DoughtyWarriors3Quest) };
-            Chains[(int)QuestChain.HonorOfDeBoors] = new[] { typeof(HonorOfDeBoorsQuest), typeof(JackTheVillainQuest), typeof(SavedHonorQuest) };
             Chains[(int)QuestChain.LaifemTheWeaver] = new[] { typeof(ShearingKnowledgeQuest), typeof(WeavingFriendshipsQuest), typeof(NewSpinQuest)};
             Chains[(int)QuestChain.CloakOfHumility] = new[] { typeof(TheQuestionsQuest), typeof(CommunityServiceMuseumQuest), typeof(CommunityServiceZooQuest), typeof(CommunityServiceLibraryQuest), typeof(WhosMostHumbleQuest) };
             Chains[(int)QuestChain.ValleyOfOne] = new[] { typeof(TimeIsOfTheEssenceQuest), typeof(UnitingTheTribesQuest) };
@@ -44,8 +40,6 @@ namespace Server.Engines.Quests
             Chains[(int)QuestChain.EodonianAlliance] = new[] { typeof(ExterminatingTheInfestationQuest), typeof(InsecticideAndRegicideQuest) };
             Chains[(int)QuestChain.FlintTheQuartermaster] = new[] { typeof(ThievesBeAfootQuest), typeof(BibliophileQuest) };
             Chains[(int)QuestChain.RightingWrong] = new[] { typeof(RightingWrongQuest2), typeof(RightingWrongQuest3), typeof(RightingWrongQuest4) };
-            Chains[(int)QuestChain.Ritual] = new[] { typeof(RitualQuest.ScalesOfADreamSerpentQuest), typeof(RitualQuest.TearsOfASoulbinderQuest), typeof(RitualQuest.PristineCrystalLotusQuest) };
-            Chains[(int)QuestChain.Ritual2] = new[] { typeof(RitualQuest.HairOfTheDryadQueen), typeof(RitualQuest.HeartOfTheNightTerror) };
         }
 
         public Type CurrentQuest { get; set; }
