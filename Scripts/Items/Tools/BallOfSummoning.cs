3b548b35179806c8f89a0ea5bdfa974f031cb6fb
TrueUO <staff@trueuo.com>
2023-09-11 10:33:31 -0400

    Update BallOfSummoning.cs

--- Scripts/Items/Tools/BallOfSummoning.cs
+++ Scripts/Items/Tools/BallOfSummoning.cs
@@ -181,7 +181,7 @@ namespace Server.Items
             {
                 MessageHelper.SendLocalizedMessageTo(this, from, 1054127, 0x22); // The Crystal Ball fills with a red mist. You appear to have let your bond to your pet deteriorate.
             }
-            else if (from.Map == Map.Ilshenar || from.Region.IsPartOf<DungeonRegion>() || from.Region.IsPartOf<Jail>() || from.Region.IsPartOf<NoTravelSpellsAllowed>())
+            else if (from.Map == Map.Ilshenar || from.Region.IsPartOf<DungeonRegion>() || from.Region.IsPartOf<Jail>() || from.Region.IsPartOf<DespiseRegion>() || from.Region.IsPartOf<NoTravelSpellsAllowed>())
             {
                 from.Send(new AsciiMessage(Serial, ItemID, MessageType.Regular, 0x22, 3, "", "You cannot summon your pet to this location."));
             }
