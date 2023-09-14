0351a7ec7cc7100aaa955843789d4aadd2ebc2e5
Daniel Knight <knight.daniel.r@gmail.com>
2023-09-14 12:09:33 -0400

    Last

--- Scripts/Spells/Third/Unlock.cs
+++ Scripts/Spells/Third/Unlock.cs
@@ -69,7 +69,7 @@ namespace Server.Spells.Third
                         else if (cont.LockLevel == 0)
                             from.SendLocalizedMessage(501666); // You can't unlock that!
                         else if (cont is HotItemChest)
-                            from.SendLocalizedMessage(501666); // You can't unlock that!
+                            from.SendLocalizedMessage(503099); // My spell does not seem to have an effect on that lock
                         else if (cont is TreasureMapChest chest && chest.Level > 2)
                             from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 503099); // My spell does not seem to have an effect on that lock.
                         else
