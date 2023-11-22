00a9489276032692b07232ed5af96bc6ba6ef0be
Daniel Knight <knight.daniel.r@gmail.com>
2023-11-21 19:20:44 -0500

    Cleanup

--- Server/HueData.cs
+++ Server/HueData.cs
@@ -1,23 +1,13 @@
-#region References
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-#endregion
 
 namespace Server
 {
     public static class HueData
     {
-#if MONO
-		public const PixelFormat PixelFormat = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
-#else
-        public const PixelFormat PixelFormat = System.Drawing.Imaging.PixelFormat.Format16bppArgb1555;
-#endif
-        private static readonly int[] m_Header = Array.Empty<int>();
+        private static readonly int[] _Header = Array.Empty<int>();
 
         public static Hue[] List { get; } = new Hue[3000];
 
@@ -42,7 +32,7 @@ namespace Server
                             blockCount = 375;
                         }
 
-                        m_Header = new int[blockCount];
+                        _Header = new int[blockCount];
 
                         int structsize = Marshal.SizeOf(typeof(HueEntry));
                         byte[] buffer = new byte[blockCount * (4 + 8 * structsize)];
@@ -61,7 +51,7 @@ namespace Server
 
                                 currpos += 4;
 
-                                m_Header[i] = (int)Marshal.PtrToStructure(ptrheader, typeof(int));
+                                _Header[i] = (int)Marshal.PtrToStructure(ptrheader, typeof(int));
 
                                 for (int j = 0; j < 8; ++j, ++index)
                                 {
@@ -89,149 +79,6 @@ namespace Server
 
                 ++index;
             }
-        }
-
-        public static Hue GetHue(int index)
-        {
-            index &= 0x3FFF;
-
-            if (index >= 0 && index < List.Length)
-            {
-                return List[index];
-            }
-
-            return List[0];
-        }
-
-        private const float ScaleF = 31f / 255f;
-
-        public static short ColorToHue(Color c)
-        {
-            ushort origred = c.R;
-            ushort origgreen = c.G;
-            ushort origblue = c.B;
-
-            ushort newred = (ushort)(origred * ScaleF);
-
-            if (newred == 0 && origred != 0)
-            {
-                newred = 1;
-            }
-
-            ushort newgreen = (ushort)(origgreen * ScaleF);
-
-            if (newgreen == 0 && origgreen != 0)
-            {
-                newgreen = 1;
-            }
-
-            ushort newblue = (ushort)(origblue * ScaleF);
-
-            if (newblue == 0 && origblue != 0)
-            {
-                newblue = 1;
-            }
-
-            return (short)((newred << 10) | (newgreen << 5) | (newblue));
-        }
-
-        private const int ScaleI = 255 / 31;
-
-        public static Color HueToColor(short hue)
-        {
-            return Color.FromArgb(((hue & 0x7c00) >> 10) * ScaleI, ((hue & 0x3e0) >> 5) * ScaleI, (hue & 0x1f) * ScaleI);
-        }
-
-        public static int HueToColorR(short hue)
-        {
-            return ((hue & 0x7c00) >> 10) * ScaleI;
-        }
-
-        public static int HueToColorG(short hue)
-        {
-            return ((hue & 0x3e0) >> 5) * ScaleI;
-        }
-
-        public static int HueToColorB(short hue)
-        {
-            return (hue & 0x1f) * ScaleI;
-        }
-
-        public static void ApplyTo(Bitmap bmp, int hue, bool onlyHueGrayPixels)
-        {
-            ApplyTo(bmp, GetHue(hue), onlyHueGrayPixels);
-        }
-
-        private static void ApplyTo(Bitmap bmp, Hue hue, bool onlyHueGrayPixels)
-        {
-            ApplyTo(bmp, hue?.Colors, onlyHueGrayPixels);
-        }
-
-        private static unsafe void ApplyTo(Bitmap bmp, IReadOnlyList<short> colors, bool onlyHueGrayPixels)
-        {
-            if (bmp == null || colors == null || colors.Count < 32)
-            {
-                return;
-            }
-
-            BitmapData bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat);
-
-            int stride = bd.Stride >> 1;
-            int width = bd.Width;
-            int height = bd.Height;
-            int delta = stride - width;
-
-            ushort* pBuffer = (ushort*)bd.Scan0;
-            ushort* pLineEnd = pBuffer + width;
-            ushort* pImageEnd = pBuffer + (stride * height);
-
-            if (onlyHueGrayPixels)
-            {
-                while (pBuffer < pImageEnd)
-                {
-                    while (pBuffer < pLineEnd)
-                    {
-                        int c = *pBuffer;
-
-                        if (c != 0)
-                        {
-                            var r = (c >> 10) & 0x1F;
-                            var g = (c >> 5) & 0x1F;
-                            var b = c & 0x1F;
-
-                            if (r == g && r == b)
-                            {
-                                *pBuffer = (ushort)colors[(c >> 10) & 0x1F];
-                            }
-                        }
-
-                        ++pBuffer;
-                    }
-
-                    pBuffer += delta;
-                    pLineEnd += stride;
-                }
-            }
-            else
-            {
-                while (pBuffer < pImageEnd)
-                {
-                    while (pBuffer < pLineEnd)
-                    {
-                        if (*pBuffer != 0)
-                        {
-                            *pBuffer = (ushort)colors[(*pBuffer >> 10) & 0x1F];
-                        }
-
-                        ++pBuffer;
-                    }
-
-                    pBuffer += delta;
-                    pLineEnd += stride;
-                }
-            }
-
-            bmp.UnlockBits(bd);
         }
 
         public sealed class Hue
@@ -272,78 +119,6 @@ namespace Server
 
                 Name = Encoding.Default.GetString(entry.Name, 0, count);
                 Name = Name.Replace("\n", " ");
-            }
-
-            public Color GetColor(int index)
-            {
-                return HueToColor(Colors[index]);
-            }
-
-            public unsafe void ApplyTo(Bitmap bmp, bool onlyHueGrayPixels)
-            {
-                BitmapData bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat);
-
-                int stride = bd.Stride >> 1;
-                int width = bd.Width;
-                int height = bd.Height;
-                int delta = stride - width;
-
-                ushort* pBuffer = (ushort*)bd.Scan0;
-                ushort* pLineEnd = pBuffer + width;
-                ushort* pImageEnd = pBuffer + (stride * height);
-
-                if (onlyHueGrayPixels)
-                {
-                    int c;
-                    int r;
-                    int g;
-                    int b;
-
-                    while (pBuffer < pImageEnd)
-                    {
-                        while (pBuffer < pLineEnd)
-                        {
-                            c = *pBuffer;
-
-                            if (c != 0)
-                            {
-                                r = (c >> 10) & 0x1F;
-                                g = (c >> 5) & 0x1F;
-                                b = c & 0x1F;
-
-                                if (r == g && r == b)
-                                {
-                                    *pBuffer = (ushort)Colors[(c >> 10) & 0x1F];
-                                }
-                            }
-
-                            ++pBuffer;
-                        }
-
-                        pBuffer += delta;
-                        pLineEnd += stride;
-                    }
-                }
-                else
-                {
-                    while (pBuffer < pImageEnd)
-                    {
-                        while (pBuffer < pLineEnd)
-                        {
-                            if (*pBuffer != 0)
-                            {
-                                *pBuffer = (ushort)Colors[(*pBuffer >> 10) & 0x1F];
-                            }
-
-                            ++pBuffer;
-                        }
-
-                        pBuffer += delta;
-                        pLineEnd += stride;
-                    }
-                }
-
-                bmp.UnlockBits(bd);
             }
         }
 
