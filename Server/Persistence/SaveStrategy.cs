5de44bdbd4f0c0b6c6015625b6ad619ff43de9b6
Daniel Knight <knight.daniel.r@gmail.com>
2023-11-24 19:05:19 -0500

    Sonar

--- Server/Persistence/SaveStrategy.cs
+++ Server/Persistence/SaveStrategy.cs
@@ -7,14 +7,9 @@ namespace Server
 {
 	public class SaveStrategy 
 	{
-        private readonly Queue<Item> _DecayQueue;
+        private readonly Queue<Item> _DecayQueue = new();
 
-		public SaveStrategy()
-		{
-			_DecayQueue = new Queue<Item>();
-		}
-
-		public void Save()
+        public void Save()
 		{
             Thread saveItemsThread = new Thread(SaveItems)
             {
@@ -42,93 +37,90 @@ namespace Server
 			}
 		}
 
-        protected void SaveMobiles()
+        private void SaveMobiles()
         {
             Dictionary<Serial, Mobile> mobiles = World.Mobiles;
 
-            using (GenericWriter idx = new BinaryFileWriter(World.MobileIndexPath, false))
-            using (GenericWriter tdb = new BinaryFileWriter(World.MobileTypesPath, false))
-            using (GenericWriter bin = new BinaryFileWriter(World.MobileDataPath, true))
+            using BinaryFileWriter idx = new(World.MobileIndexPath, false);
+            using BinaryFileWriter tdb = new(World.MobileTypesPath, false);
+            using GenericWriter bin = new BinaryFileWriter(World.MobileDataPath, true);
+
+            idx.Write(mobiles.Count);
+            foreach (Mobile m in mobiles.Values)
             {
-                idx.Write(mobiles.Count);
-                foreach (Mobile m in mobiles.Values)
-                {
-                    long start = bin.Position;
+                long start = bin.Position;
 
-                    idx.Write(m.m_TypeRef);
-                    idx.Write(m.Serial);
-                    idx.Write(start);
+                idx.Write(m.m_TypeRef);
+                idx.Write(m.Serial);
+                idx.Write(start);
 
-                    m.Serialize(bin);
+                m.Serialize(bin);
 
-                    idx.Write((int)(bin.Position - start));
+                idx.Write((int)(bin.Position - start));
 
-                    m.FreeCache();
-                }
+                m.FreeCache();
+            }
 
-                tdb.Write(World.m_MobileTypes.Count);
-                for (int i = 0; i < World.m_MobileTypes.Count; ++i)
-                {
-                    tdb.Write(World.m_MobileTypes[i].FullName);
-                }
+            tdb.Write(World.m_MobileTypes.Count);
+            for (int i = 0; i < World.m_MobileTypes.Count; ++i)
+            {
+                tdb.Write(World.m_MobileTypes[i].FullName);
             }
         }
 
-        protected void SaveItems()
+        private void SaveItems()
         {
             Dictionary<Serial, Item> items = World.Items;
 
-            using (GenericWriter idx = new BinaryFileWriter(World.ItemIndexPath, false))
-            using (GenericWriter tdb = new BinaryFileWriter(World.ItemTypesPath, false))
-            using (GenericWriter bin = new BinaryFileWriter(World.ItemDataPath, true))
+            using BinaryFileWriter idx = new(World.ItemIndexPath, false);
+            using BinaryFileWriter tdb = new(World.ItemTypesPath, false);
+            using GenericWriter bin = new BinaryFileWriter(World.ItemDataPath, true);
+
+            idx.Write(items.Count);
+            foreach (Item item in items.Values)
             {
-                idx.Write(items.Count);
-                foreach (Item item in items.Values)
+                if (item.Decays && item.Parent == null && item.Map != Map.Internal && (item.LastMoved + item.DecayTime) <= DateTime.UtcNow)
                 {
-                    if (item.Decays && item.Parent == null && item.Map != Map.Internal && (item.LastMoved + item.DecayTime) <= DateTime.UtcNow)
-                    {
-                        _DecayQueue.Enqueue(item);
-                    }
-
-                    long start = bin.Position;
-
-                    idx.Write(item.m_TypeRef);
-                    idx.Write(item.Serial);
-                    idx.Write(start);
-
-                    item.Serialize(bin);
-
-                    idx.Write((int)(bin.Position - start));
-
-                    item.FreeCache();
+                    _DecayQueue.Enqueue(item);
                 }
 
-                tdb.Write(World.m_ItemTypes.Count);
-                for (int i = 0; i < World.m_ItemTypes.Count; ++i)
-                {
-                    tdb.Write(World.m_ItemTypes[i].FullName);
-                }
+                long start = bin.Position;
+
+                idx.Write(item.m_TypeRef);
+                idx.Write(item.Serial);
+                idx.Write(start);
+
+                item.Serialize(bin);
+
+                idx.Write((int)(bin.Position - start));
+
+                item.FreeCache();
+            }
+
+            tdb.Write(World.m_ItemTypes.Count);
+            for (int i = 0; i < World.m_ItemTypes.Count; ++i)
+            {
+                tdb.Write(World.m_ItemTypes[i].FullName);
             }
         }
 
-        protected void SaveGuilds()
+        private void SaveGuilds()
         {
-            using (GenericWriter idx = new BinaryFileWriter(World.GuildIndexPath, false))
-            using (GenericWriter bin = new BinaryFileWriter(World.GuildDataPath, true))
+            using BinaryFileWriter idx = new(World.GuildIndexPath, false);
+            using GenericWriter bin = new BinaryFileWriter(World.GuildDataPath, true);
+
+            idx.Write(BaseGuild.List.Count);
+            foreach (BaseGuild guild in BaseGuild.List.Values)
             {
-                idx.Write(BaseGuild.List.Count);
-                foreach (BaseGuild guild in BaseGuild.List.Values)
-                {
-                    long start = bin.Position;
+                long start = bin.Position;
 
-                    idx.Write(0);//guilds have no typeid
-                    idx.Write(guild.Id);
-                    idx.Write(start);
+                idx.Write(0);//guilds have no typeid
+                idx.Write(guild.Id);
+                idx.Write(start);
 
-                    guild.Serialize(bin);
+                guild.Serialize(bin);
 
-                    idx.Write((int)(bin.Position - start));
-                }
+                idx.Write((int)(bin.Position - start));
             }
         }
     }
