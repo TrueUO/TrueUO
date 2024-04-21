a4110758e3a169e19cd8218cef7c36730fc4d2e8
adverserath <adverserath@gmail.com>
2024-04-22 00:08:35 +0100

    Fix Target type on items Fix instrument selection on bard instant target

--- Scripts/Items/Equipment/Spellbooks/Spellbook.cs
+++ Scripts/Items/Equipment/Spellbooks/Spellbook.cs
@@ -347,7 +347,7 @@ namespace Server.Items
                             }
                             else if (toI != null)
                             {
-                                spell.InstantTarget = toI as IDamageableItem;
+                                spell.InstantTarget = toI as IEntity;
                             }
                             spell.Cast();
                         }
